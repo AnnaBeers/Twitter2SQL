8c325762b3473e6c968ff8f8845c486f98b20c6e
ANDREW BEERS <albeers@earth.lab.cip.uw.edu>
2020-07-08 19:25:00 +0000

    docs: Requirements and documentation for trees

--- twitter2sql/analysis/trees.py
+++ twitter2sql/analysis/trees.py
@@ -1,3 +1,8 @@
+""" This module focuses on functions which pull tree-structured data, such as reply threads,
+and subsequently modifies them.
+"""
+
+
 import pickle
 import os
 import pandas as pd
@@ -31,45 +36,80 @@ def export_reply_threads(database_name,
         seed_table_name=None,
         seed_limit=500,
         seed_random_percent=None,
-        reply_range=[1, 5],
+        reply_range=[1, 10000000000000],
         verbose=False,
         output_type='csv',
         output_filepath=None):
 
+
+    """ If not seed database is provided, we assume that you are pulling your seed posts
+    from the same database your are pulling replies from.
+    """
     if seed_database_name is None:
         seed_database_name = database_name
     if seed_db_config_file is None:
         seed_db_config_file = db_config_file
 
+    """ The cursor is a curious concept in SQL. All queries need to run through the cursor
+    object, and you need to generate one with Python. I use a function in twitter2sql.core.util
+    to do this easily. You need a unique cursor for each server/database combination.
+    """
     seed_database, seed_cursor = open_database(seed_database_name, seed_db_config_file)
     database, cursor = open_database(database_name, db_config_file)
 
+    """ I have a variable, select_columns, which determines which columns users want returned in
+    this process. The 'id' column, however, is necessary for this whole operation to work, so I
+    add it here as a check on the user (me) who might forget it.
+    """
     if 'id' not in select_columns:
         select_columns = select_columns + ['id']
 
+    """ Step 1, get the seed posts! Check the function below this one for additional documentation.
+    """
     seed_posts = get_seed_posts(seed_cursor, seed_limit, seed_conditions, select_columns,
-            seed_random_percent, seed_table_name)
+            seed_random_percent, seed_table_name, verbose)
 
+    """ A bit of magic is done here to make sure that 'id' and 'in_reply_to_status_id'
+    are in select_columns, and makes sure that the 'id' column is the first one in
+    select_columns. This is over-complicated, and should probably be revised later :).
+    """
+    if 'id' not in select_columns:
+        select_columns = select_columns + ['in_reply_to_status_id']
+    select_columns.insert(0, select_columns.pop(select_columns.index('id')))
+
+    """ Step 2, retrieve the replies and write them to a csv document.
+    """
     if output_type == 'csv':
         with open(output_filepath, 'w') as openfile:
+
             writer = csv.writer(openfile, delimiter=',')
             writer.writerow(select_columns + ['path', 'depth', 'is_seed'])
      
             for seed_post in tqdm(seed_posts):
 
-                results = get_reply_thread(cursor, seed_post, table_name, select_columns,
-                        reply_range)
-
+                """ First, write the row for your seed post. The last three columns are only applicable
+                    for replies, so fill them with dummy values.
+                """
                 writer.writerow([seed_post[key] for key in select_columns] + ['NONE', 1, 'TRUE'])
 
+                """ Then, pull the replies! See the get_reply_thread function for more.
+                """
+                results = get_reply_thread(cursor, seed_post, table_name, select_columns,
+                        reply_range, verbose)
+
+                """ IF there are replies, this recursive function will write them to your csv-file in
+                    'reply order.' In other words, replies will be threaded as they are in e.g. Reddit.
+                """
                 if results:
                     for idx, result in enumerate(results):
-                        if result['depth'] == 1 or result['depth'] > 2:
-                            continue
-                        else:
+                        if result['depth'] == 2:
                             construct_tree(result, results, idx, writer, select_columns)
 
     elif output_type == 'networkx':
+
+        """ This code segment is for pulling out data into networkx format. It will be documented later :)
+        """
+
         with open(output_filepath, 'wb') as openfile:
 
             thread_networks = []
@@ -85,9 +125,7 @@ def export_reply_threads(database_name,
                 if results:
                     for idx, result in enumerate(results):
                         G.add_node(result['id'], time=result['created_at'], user_id=result['user_id'], depth=result['depth'])
-                        # print(G.nodes(data=True))
                         G.add_edge(result['id'], result['in_reply_to_status_id'])
-                        # print(G.nodes(data=True))
 
                 thread_networks += [G]
 
@@ -95,7 +133,17 @@ def export_reply_threads(database_name,
 
 
 def get_seed_posts(cursor, seed_limit, seed_conditions, 
-            select_columns, seed_random_percent, seed_table_name):
+            select_columns, seed_random_percent, seed_table_name,
+            verbose=False):
+
+    """ Creates and executes a simple SQL statement to retrieve posts from
+    a table subject to certain conditions. We take advantage of the 'format'
+    function to insert variables into a SQL template defined below. Note that
+    we have to use psycopg2's "sql.SQL" function to wrap our inserted variables,
+    or else it will get mad at us. For more details on why they require this,
+    see Paul's comments about SQL injection -- although I don't think I have
+    entirely prevented SQL injection here.
+    """
 
     seed_limit = sql_statements.limit(seed_limit) 
     select_columns_sql = sql_statements.select_cols(select_columns)
@@ -111,7 +159,10 @@ def get_seed_posts(cursor, seed_limit, seed_conditions,
                 seed_limit=seed_limit,
                 seed_conditions=seed_conditions,
                 select_columns=select_columns_sql)
-    print(sql_statement.as_string(cursor))
+    
+    # This will print the SQL statement as-computed, so you can test it separately.
+    if verbose:
+        print(sql_statement.as_string(cursor))
 
     cursor.execute(sql_statement)  
     seed_posts = to_list_of_dicts(cursor)
@@ -120,19 +171,14 @@ def get_seed_posts(cursor, seed_limit, seed_conditions,
 
 
 def get_reply_thread(cursor, seed_post, table_name, select_columns,
-            reply_range):
-
-    post_id = seed_post['id']
-    # print(seed_post)
-
-    for col in ['id', 'in_reply_to_status_id']:
-        if col not in select_columns:
-            select_columns += [col]
-    select_columns.insert(0, select_columns.pop(select_columns.index('id')))
+            reply_range, verbose):
 
     select_columns_sql = sql_statements.select_cols(select_columns)
     select_columns_child = sql_statements.select_cols(['c.' + col for col in select_columns])
 
+    """ For the SQL command below, we will need to insert data types for each of our databases
+    tweet columns. This bit of code creates the requisite SQL statement for this purpose.
+    """
     types = sql_type_dictionary()
     column_types = ''
     for col in select_columns:
@@ -142,6 +188,15 @@ def get_reply_thread(cursor, seed_post, table_name, select_columns,
             column_types += 'NULL::{} AS {},'.format(types[col], col)
     column_types = sql.SQL(column_types)
 
+    """ This SQL template is where the magic happens. This is a recursive query. You give it
+        one seed post, which we insert manually aboe the UNION ALL command, and use that to
+        pull all posts that reply to that seed post. Then, you take each reply that you've
+        captured, and run the same query again for each of them, getting each of THEIR replies.
+        You do this until your queries are no longer returning results, thus going down the entire
+        tree of replies for an original seed post.
+    """
+
+    post_id = seed_post['id']
     sql_statement = sql.SQL("""
         WITH RECURSIVE recursive_tweets({select_columns}, depth, path) AS (
         SELECT {seed_post}::bigint AS id, 
@@ -157,10 +212,22 @@ def get_reply_thread(cursor, seed_post, table_name, select_columns,
                 select_columns=select_columns_sql, select_columns_child=select_columns_child,
                 column_types=column_types)
 
+    """ The template is hard to understand -- running with verbose=True will let you see the
+    command with variables inserted.
+    """
+    if verbose:
+        print(sql_statement.as_string(cursor))
+
     cursor.execute(sql_statement)
     results = to_list_of_dicts(cursor)
+
+    """ The first result is the seed post, which we don't need here.
+    """
     results.pop(0)
 
+    """ If you wanted to exclude posts with few (or too many) replies,
+    you can use the reply_range variable here.
+    """
     results_length = len(results)
     if reply_range is not None:
         if reply_range[0] <= results_length <= reply_range[1]:
@@ -172,6 +239,12 @@ def get_reply_thread(cursor, seed_post, table_name, select_columns,
 
 
 def construct_tree(row, results, idx, writer, select_columns):
+
+    """ A recursive function (!!) that reorganizes depth-sorted lists of tweets
+    into thread-order, so that replies are always as close to their parents as
+    possible.
+    """
+
     depth = row['depth']
     parent_id = row['in_reply_to_status_id']
     writer.writerow([row[key] for key in select_columns + ['path', 'depth']] + ['FALSE'])
@@ -182,31 +255,6 @@ def construct_tree(row, results, idx, writer, select_columns):
             construct_tree(result, results, idx + sub_idx, writer, select_columns)
 
     return
-
-
-"""
-    old_statment = sql.SQL(EXPLAIN
-                WITH RECURSIVE tweets_cte(id, user_id, tweet, user_name, user_screen_name,
-                parent_id,
-                created_at, in_reply_to_user_screen_name,
-                in_reply_to_user_id, depth, path) AS (
-                SELECT {seed_post}::bigint AS id, NULL::bigint AS user_id,
-                NULL::TEXT as tweet, 
-                NULL::TEXT as user_name, NULL::TEXT as user_screen_name,
-                NULL::bigint AS in_reply_to_status_id,
-                NULL::timestamptz AS created_at, NULL::text as in_reply_to_user_screen_name,
-                NULL::bigint as in_reply_to_user_id, 1::INT AS depth,
-                {seed_post}::TEXT AS path
-                UNION ALL
-                SELECT c.id, c.user_id, c.tweet, c.user_name, c.user_screen_name, c.in_reply_to_status_id, c.created_at, c.in_reply_to_user_screen_name,
-                c.in_reply_to_user_id, p.depth + 1 AS depth, (p.path || '->' || c.id::TEXT) 
-                FROM tweets_cte AS p, {table_name} AS c WHERE c.in_reply_to_status_id = p.id
-                )
-                SELECT * FROM tweets_cte AS n;
-                ).format(table_name=sql.SQL(table_name), seed_post=sql.SQL(str(post_id)),
-                select_columns=select_columns_sql, select_columns_child=select_columns_child,
-                column_types=column_types)
-                """
 
 
 if __name__ == '__main__':
